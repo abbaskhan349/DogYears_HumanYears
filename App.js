75e0e61d89d9193d106b2df8acb383e57810b4cc
Muhammad Abbas Khan <100797911+abbaskhan349@users.noreply.github.com>
2022-12-31 02:14:50 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+const myAge = 22;
+//declaring age variable and assigning the age
+
+let earlyYears = 2;
+//the early two years of the dogs
+
+earlyYears *= 10.5;
+
+var laterYears = myAge - 2;
+
+laterYears *= 4;
+//
+
+console.log(earlyYears);
+console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;
+//
+
+var myName = "Abbas".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+//answer will be 101(as of 22)
